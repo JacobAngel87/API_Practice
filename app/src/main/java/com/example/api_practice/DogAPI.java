522b0df5cdaf0d9040c5f2b32c9ffe4274b552cc
Jacob Angel <jacobryanangel@gmail.com>
2021-04-05 13:47:12 -0500

    Working Version.

--- app/src/main/java/com/example/api_practice/DogAPI.java
+++ app/src/main/java/com/example/api_practice/DogAPI.java
@@ -1,45 +1,26 @@
 package com.example.api_practice;
 
+import android.app.Application;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DogAPI {
-    private StringRequest apiCall;
-    private final String url = "https://randomuser.me/api";
-    private JSONObject results;
 
     public DogAPI() {
-        getNewDog();
-    }
 
-    public void getNewDog() {
-        apiCall = new StringRequest(Request.Method.GET, url,
-                response -> {
-                    try {
-                        results = new JSONObject(response).getJSONObject("list");
-                        Log.e("JSON DATA", "" + results);
-                    }
-                    catch (JSONException e) {
-                        // TODO Error Catching
-                        Log.e("ERROR", "JSON Data Parsing Failed");
-                    }
-                },
-                error -> {
-                    Log.e("ERROR", "API Request Error");
-                }
-        );
-    }
-
-    public String getDogImg() throws JSONException {
-        if(results == null) return "";
-        return results.getString("message");
     }
 }
--- app/src/main/java/com/example/api_practice/MainActivity.java
+++ app/src/main/java/com/example/api_practice/MainActivity.java
@@ -3,29 +3,49 @@ package com.example.api_practice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String data;
     private ImageView dogImg;
-    private DogAPI dogAPI;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dogImg = findViewById(R.id.imgDog);
-        dogAPI = new DogAPI();
-        /*try {
-            Picasso.get().load(dogAPI.getDogImg()).into(dogImg);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } */
+        final TextView textView = (TextView) findViewById(R.id.txt_data);
 
-        //TODO Test commits to master branch
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url ="https://dog.ceo/api/breeds/image/random";
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> {
+                    try {
+                        data = new JSONObject(response).getString("message");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    textView.setText(data);
+                    Picasso.get().load(data).into(dogImg);
+                }, error -> textView.setText("That didn't work!"));
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
