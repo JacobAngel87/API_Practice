46ed3c6b7454265ceb6bd249eef81aff0553c139
ja0923920 <jacobryanangel@gmail.com>
2021-04-05 09:30:22 -0500

    Added GET API code.

--- app/src/main/java/com/example/api_practice/DogAPI.java
+++ app/src/main/java/com/example/api_practice/DogAPI.java
@@ -1,5 +1,44 @@
 package com.example.api_practice;
 
-public class DogAPI {
+import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DogAPI {
+    private StringRequest apiCall;
+    private final String url = "https://dog.ceo/api/breeds/image/random";
+    private JSONObject results;
+
+    public DogAPI() {
+        getNewDog();
+    }
+
+    public void getNewDog() {
+        apiCall = new StringRequest(Request.Method.GET, url,
+                response -> {
+                    try {
+                        results = new JSONObject(response).getJSONObject("list");
+                    }
+                    catch (JSONException e) {
+                        // TODO Error Catching
+                        Log.e("ERROR", "JSON Data Parsing Failed");
+                    }
+                },
+                error -> {
+                    Log.e("ERROR", "API Request Error");
+                }
+        );
+    }
+
+    public String getDogImg() throws JSONException {
+        if(results == null) return "";
+        return results.getString("message");
+    }
 }
