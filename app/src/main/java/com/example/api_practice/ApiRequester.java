b192538b731dce578f5a78e58054beda95e60d92
CarlCaldwell <cc0217891@otc.edu>
2021-04-08 18:32:44 -0500

    Removed unused Imports

--- app/src/main/java/com/example/api_practice/ApiRequester.java
+++ app/src/main/java/com/example/api_practice/ApiRequester.java
@@ -5,6 +5,9 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApiRequester {
 
     /* We need an instance of the MainActivity so
@@ -17,6 +20,10 @@ public class ApiRequester {
     // The data returned from the API stored goes into this variable
     private String data;
 
+    private String id;
+    private String topText;
+    private String bottomText;
+
     // Constructor defines the queue that we created
     // Uses the MainActivity for context
     public ApiRequester(MainActivity main) {
@@ -27,7 +34,7 @@ public class ApiRequester {
     // Requests an API call from the user with a URL provided as a parameter.
     /* Uses the VolleyCallBack interface to allow the user to execute code after
          the call has completed inside of the onSuccess method */
-    public void requestAPI(String url, final VolleyCallBack callBack) {
+    public void requestAPIGet(String url, final VolleyCallBack callBack) {
         stringRequest = new StringRequest(Request.Method.GET, url,
                 // If the data was returned from the API
                 response -> {
@@ -37,13 +44,53 @@ public class ApiRequester {
                     callBack.onSuccess(data);
                 },
                 // If there was no data returned
-                error -> data = null);
+                error -> {
+                    data = null;
+                    error.printStackTrace();
+                });
         // After we create our API request add it to the queue
         // (Note that 'add' also starts the queue)
         queue.add(stringRequest);
     }
+    public void requestAPIPost(String url, final VolleyCallBack callBack) {
+        stringRequest = new StringRequest(Request.Method.POST, url,
+                // If the data was returned from the API
+                response -> {
+                    // Store the data returned
+                    data = response;
+                    // Callback the onSuccess method
+                    callBack.onSuccess(data);
+                },
+                // If there was no data returned
+                error -> {
+                    data = null;
+                    error.printStackTrace();
+                }){
+            // Adds the http parameters to the post request
+            protected Map<String,String> getParams() {
+                Map<String,String> postData = new HashMap<>();
+                postData.put("template_id", id);
+                postData.put("username", "JacobAngel87");
+                postData.put("password", "PizzaMan87!");
+                postData.put("text0", topText);
+                postData.put("text1", bottomText);
+                return postData;
+            }
+        };
+        // After we create our API request add it to the queue
+        // (Note that 'add' also starts the queue)
+        queue.add(stringRequest);
+    }
+
     // Returns the data from the API
     public String getData() {
         return data;
+    }
+
+    // Need to call this method before calling the requestAPIPost
+    public void addParametersToPost(int id, String topText, String bottomText) {
+        this.id = String.valueOf(id);
+        this.topText = topText;
+        this.bottomText = bottomText;
     }
 }
