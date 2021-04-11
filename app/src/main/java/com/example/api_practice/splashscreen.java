efba58d7f99231549c3f10107f932212c60a8cc1
Beth423 <59936151+Beth423@users.noreply.github.com>
2021-04-11 13:33:01 -0500

    added splash screen imge

--- app/src/main/java/com/example/api_practice/splashscreen.java
+++ app/src/main/java/com/example/api_practice/splashscreen.java
@@ -1,16 +1,19 @@
 package com.example.api_practice;
 
 import android.content.Intent;
+
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class splashscreen extends AppCompatActivity {
     @Override
-    protected void onCreate (Bundle savedinstanceStat){
-        super.onCreate(savedinstanceStat);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
-   }
+    protected  void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+             Intent intent = new Intent(this , MainActivity.class);
+             startActivity(intent);
+             finish();;
+
+    }
+
 }
